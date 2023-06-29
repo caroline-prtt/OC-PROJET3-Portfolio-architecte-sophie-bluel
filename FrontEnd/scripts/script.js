4d6adad312526437a0644e5aebc4a1a911abc771
Caroline Protat <caroline.protat@hotmail.fr>
2023-06-29 17:15:27 +0200

    boucle générant les projets a été intégrée dans une fonction + création des filtres en HTML/CSS et affichage dynamique des travaux filtrés avec JS

--- FrontEnd/scripts/script.js
+++ FrontEnd/scripts/script.js
@@ -1,39 +1,90 @@
-// ***********************************
-// RÉCUPÉRATION DES PROJETS VIA L'API
-// ***********************************
-
-// On définit la fonction asynchrone gallery : créée pour générer les projets
-// grâce à la requête GET/works à l'API
+// Fonction async qui englobe tout le code qui dépend de la requête GET/works de l'API
 
 async function gallery() {
 
-    // Récupérer des donneés de l'API : requête GET/works
+    // REQUETE API : GET/WORKS
+    // ***********************
+
     const reponse = await fetch("http://localhost:5678/api/works");
     const works = await reponse.json();
     
-    // Création de la boucle pour créer chaque fiche projet
+    // FONCTION COMPRENANT LA BOUCLE QUI GÉNÈRE LES PROJETS
+    // *********************************************************
 
-    for (let i = 0; i < works.length; i++) {
+    function genereWorks (works){
 
-        // récupère l'élément du DOM qui accueillera les travaux
-        const divGallery = document.querySelector(".gallery");
+        for (let i = 0; i < works.length; i++) {
 
-        // On créé la balise dédié à une fiche travaux
-        const workElement = document.createElement("figure");
-        // On créé la balise image des travaux
-        const imageElement = document.createElement("img");
-        // On accède à l'indice i de la liste de travaux pour configurer la source de l'image
-        imageElement.src = works[i].imageUrl;
-        // on créé la balise figcaption pour le titre des travaux
-        const titreElement = document.createElement("figcaption");
-        // On accède à l'indice i de la liste travaux pour configurer le titre
-        titreElement.textContent = works[i].title;
+            // récupère l'élément du DOM qui accueillera les travaux
+            const divGallery = document.querySelector(".gallery");
 
-        // On rattache tous les éléments à leur élément parent
-        divGallery.appendChild(workElement);
-        workElement.appendChild(imageElement);
-        workElement.appendChild(titreElement);
+            // On créé la balise dédié à une fiche travaux
+            const workElement = document.createElement("figure");
+            // On créé la balise image des travaux
+            const imageElement = document.createElement("img");
+            // On accède à l'indice i de la liste de travaux pour configurer la source de l'image
+            imageElement.src = works[i].imageUrl;
+            // on créé la balise figcaption pour le titre des travaux
+            const titreElement = document.createElement("figcaption");
+            // On accède à l'indice i de la liste travaux pour configurer le titre
+            titreElement.textContent = works[i].title;
+
+            // On rattache tous les éléments à leur élément parent
+            divGallery.appendChild(workElement);
+            workElement.appendChild(imageElement);
+            workElement.appendChild(titreElement);
+        }
     }
+
+    genereWorks(works);  // appel de la fonction genereWorks pour exécuter son contenu
+
+    // RÉALISATION DES FILTRES D'AFFICHAGE
+    // ***********************************
+
+    // Filtre objets
+
+    const filterObjets = document.querySelector("#filter-objets");
+    filterObjets.addEventListener("click", function() {
+        const worksObjets = works.filter(function(work){
+            return work.categoryId == 1;
+        });
+        document.querySelector(".gallery").innerHTML = "";
+        genereWorks(worksObjets);
+    });
+
+    // Filtre appartements
+
+    const filterAppartements = document.querySelector("#filter-appartements");
+    filterAppartements.addEventListener("click", function() {
+        const worksAppartements = works.filter(function(work){
+            return work.categoryId == 2;
+        });
+        document.querySelector(".gallery").innerHTML = "";
+        genereWorks(worksAppartements);
+    });
+
+    // Filtre hotels & restaurants
+
+    const filterHotels = document.querySelector("#filter-hotels");
+    filterHotels.addEventListener("click", function() {
+        const worksHotels = works.filter(function(work){
+            return work.categoryId == 3;
+        });
+        document.querySelector(".gallery").innerHTML = "";
+        genereWorks(worksHotels);
+    });
+
+    // Filtre tous
+
+    const filterTous = document.querySelector("#filter-tous");
+
+    filterTous.addEventListener("click",function(){
+        const worksTous = works.filter(function(work){
+            document.querySelector(".gallery").innerHTML = "";
+            genereWorks(works);
+        })
+    });
+
 }
 
 // Puis appel de la fonction Portfolio pour générer le code qu'elle contient
