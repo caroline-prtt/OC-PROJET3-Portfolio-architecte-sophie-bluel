ebfabfaf2db53f0d6a92ee302d2aeef137c2120a
Caroline Protat <caroline.protat@hotmail.fr>
2023-06-30 13:12:51 +0200

    création du fichier loging.js

---
+++ FrontEnd/scripts/login.js
@@ -0,0 +1,21 @@
+/* 
+
+
+let admin = {
+    email:
+    password:
+}
+
+async{
+    
+    1- faire requete fetch: post/User
+        method
+        body
+        headers
+   
+    2- créer condition :
+    if (bon email/password)
+    else (mauvais emailet/ou/password) 
+    
+    
+    */
