62d2fda06fb42935084492ff7c2ccd08d3e3858a
Caroline Protat <caroline.protat@hotmail.fr>
2023-07-05 13:50:28 +0200

    création du fichier loging.js

--- FrontEnd/scripts/login.js
+++ FrontEnd/scripts/login.js
@@ -1,10 +1,16 @@
+
+// Récupère les valeurs du formulaire Login:
+
+let email = document.getElementById("email").value;
+let password = document.getElementById("password").value;
+
+console.log(email);
+console.log(password);
+
+
+
 /* 
 
-
-let admin = {
-    email:
-    password:
-}
 
 async{
     
@@ -14,8 +20,14 @@ async{
         headers
    
     2- créer condition :
-    if (bon email/password)
-    else (mauvais emailet/ou/password) 
+    if (email==="sophie@"" && password ==="") {
+        - renvoi page accueil administrateur)
+    }
+    else (mauvais emailet/ou/password) {
+        - message d'erreur
+    }
     
     
+voir question localStorage
+
     */
