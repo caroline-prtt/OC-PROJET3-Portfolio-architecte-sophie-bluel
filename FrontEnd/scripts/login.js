8fcac0324642628f42c5ca1b54f3b2be5cb6d438
Caroline Protat <caroline.protat@hotmail.fr>
2023-07-06 20:30:34 +0200

    intégration du script pour gérer l'évènement submit : connexion de l'utilisateur avec renvoi à la page index.html ou affichage message d'erreur

--- FrontEnd/scripts/login.js
+++ FrontEnd/scripts/login.js
@@ -1,60 +1,90 @@
-// On récupère la balise form
+// 1_ ON RÉCUPÈRE LA BALISE FORM
 
-let formLogIn = document.getElementById("formLogIn");
+const formLogIn = document.getElementById("formLogIn");
 
-// On gère l'évènement submit du formulaire LogIn
+// 2_ GESTION DE L'ÉVÈNMENT "SUBMIT" DU FORMULAIRE LOGIN
+// A SAVOIR > CONNEXION OU NON DE L'UTILISATEUR AVEC REQUETE API
 
-formLogIn.addEventListener("submit", (event) => {
+/* Fonction anonyme déclarée comme "async" car contient du code faisant appel
+ à une fonction fetch utilisant le mot clé "await" (permet d'attendre la 
+résolution de la promesse renvoyée par fetch avant de continuer exécution code) */
+
+formLogIn.addEventListener("submit", async function (event) {
+
     // On commence par supprimer le comportement par défaut (raffraichit la page)
     event.preventDefault();
-    // Récupère les valeurs inscrites dans le formulaire Login:
+    
+    // Récupère les valeurs inscrites par l'utilisateur dans le formulaire:
     let email = document.getElementById("email").value;
     let password = document.getElementById("password").value;
 
-    console.log(email);
-    console.log(password);
-})
+        // console.log(email);
+        // console.log(password);
 
-
-
-
-/* 
-
-1_ création du formulaire LogIn HTML
-
-2_ ajouter l'eventListener sur l'événement "submit" de la balise form 
-    > penser a supprimer le comportement par défaut de submit
-
-3_ Créer la charge utile de la requête : 
+    // Création de la charge utile de la requête :
     const users = {
-        "email":   ,
-        "passeword"
+        "email": email,
+        "password": password
+    };
+
+        // console.log(users);
+
+    // REQUETE A L'API : connexion ou non de l'utilisateur
+
+    try{
+        
+        const response = await fetch ("http://localhost:5678/api/users/login",{
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(users)
+        } )
+            // console.log(response);
+
+        // extraction de "response" au format JSON
+        const usersLogin = await response.json();
+         
+         /* "".ok" : est l'une des propriétés de l'objet "response" générée 
+        par la promesse renvoyée par la fonction fetch (visible avec console.log(response))
+        et "ok: true" si email et password corrects, sinon est "false" */
+        if (response.ok === true){
+
+            // on stock le token renvoyé par requête (format réponse.json vu dans Swagger)
+            const token = usersLogin.token;
+            // puis on le stock dans le sessionStorage (supprimé après fermeture navigateur)
+            // setItem("clé", valeur)
+            sessionStorage.setItem("token", token);
+            // redirection vers la page d'accueil
+            window.location.href = "index.html";
+        }
+
+        // Gestion des erreurs de connexion 
+
+        else {
+            
+            // Récupère div du formulaire qui a class ".errorLogIn" dans laquelle 
+            // on fera apparaitre le message d'erreur
+            let divErrorMessage = document.querySelector(".errorLogIn");
+            
+            // Modification de la class de la div
+            // Nouvelle class
+            divErrorMessage.classList.add("errorLogInMessage");
+            // Suppression ancienne class
+            divErrorMessage.classList.remove("errorLogIn");
+            // Insertion balise HTML
+            divErrorMessage.innerHTML = "<p>Erreur dans l’identifiant ou le mot de passe</p>";
+
+            // Puis propriétés css intégrées dans styles.css pour la div avec nouvelle class=errorLogInMessage
+        }
+
     }
 
-4_ Convertir l'objet au format JSON avec JSON.strigify();
-
-5_ Faire la requête à l'API
-
-async function LogIn() {
-
-    par exemple fetch("http://localhost:5678/api/users/login", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: '{
-  "email": "string",
-  "password": "string"
-}'
-});
-
-   créer condition :
-    if (email==="sophie@"" && password ==="") {
-        - renvoi page accueil administrateur)
+    // Gestion des erreurs de REQUETE avec "catch"
+    catch{
+        console.log("erreur dans la requête API")
     }
-    else (mauvais emailet/ou/password) {
-        - message d'erreur
-    }
-    
-    
-voir question localStorage et token
+          
+});        
 
-    */
+// 3_ RÉCUPÈRE LE TOKEN
+// Par le biais de Session Storage, à l'extérieur de la async function (event).
+const token = sessionStorage.getItem("token");
