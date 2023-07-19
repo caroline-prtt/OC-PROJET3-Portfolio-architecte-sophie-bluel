2342c37a46a61e368c7338e2c82e2df2f08daef7
Caroline Protat <caroline.protat@hotmail.fr>
2023-07-19 16:50:57 +0200

    apparition barre d'édition à la connexion de l'administrateur

--- FrontEnd/scripts/admin.js
+++ FrontEnd/scripts/admin.js
@@ -7,3 +7,15 @@ const token = sessionStorage.getItem("token");
 
 console.log(token);
 
+// APPARITION DES NOUVEAUX MENUS EDITIONS SUR LA PAGE ACCUEIL
+
+// selectionner la div
+
+if(token !== null){
+    console.log("Connecté !");
+    let menuEdition = document.querySelector(".wait-connexion");
+    menuEdition.classList.add("administrateur")
+    menuEdition.classList.remove("wait-connexion")
+} else {
+    console.log("Pas connecté");
+};
