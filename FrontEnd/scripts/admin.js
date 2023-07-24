002a8b5967ed19dba85499afb5f45df2acc56fdf
Caroline Protat <caroline.protat@hotmail.fr>
2023-07-24 14:19:20 +0200

    ajout du script pour l'ouverture des boites modales lors du click sur les différents liens ou icone retour, avec dissimulation de la boite modale non active

--- FrontEnd/scripts/admin.js
+++ FrontEnd/scripts/admin.js
@@ -36,3 +36,71 @@ if(token !== null){
 } else {
     console.log("Pas connecté");
 }
+
+
+// ******************************
+// EXÉCUTION DES FENETRES MODALES
+// ******************************
+
+// GÉNÉRAL
+// -------
+
+// lien "modifier" ouvrant modale 1
+const linkOpenModal1 = document.querySelector(".js-open-modal1");
+
+// lien "ajouter photo" ouvrant modale 2
+const linkOpenModal2 = document.querySelector(".js-open-modal2");
+
+// lien icone "flèche retour" ouvrant la modale 1
+const linkReturnModal1 = document.querySelector(".js-return-modal1");
+
+// le conteneur modale 1 <aside>
+const modal1 = document.getElementById("modal1");
+
+// le conteneur modale 1 <aside>
+const modal2 = document.getElementById("modal2");
+
+
+// OUVERTURE DES MODALES
+// ---------------------
+
+function openModal(event, target){
+    event.preventDefault();
+    if (target === modal1){
+        modal1.style.display = null;
+        modal2.style.display = "none";
+    } else {
+        modal1.style.display = "none";
+        modal2.style.display = null;
+    }
+};
+
+linkOpenModal1.addEventListener("click", function (event){
+    openModal(event, modal1);
+})
+
+linkOpenModal2.addEventListener("click", function(event){
+    openModal(event, modal2);
+})
+
+linkReturnModal1.addEventListener("click", function(event){
+    openModal(event, modal1);
+})
+
+/* 
+    2 - FERMETURE MODALE : gérer fermeture de la modale au clic sur le bouton croix ou
+    lorsqu'on clique à coté de la modale
+
+    3 - APPARITION PROJETS : photos des projets avec pour chacun
+        _ une icone corbeille permettant de supprimer le projet (chaque bouton
+            doit avoir son propre id pour reconnaitre le projet supprimée)
+        _ un bouton éditer
+
+    4 - Le bouton ajouter un projet pour passer à la modale 2 (AJOUTER LIEN & ANCRE)
+
+    5 - AJOUT DE PROJET : modale ajout de projet : 
+        _ apparition de la miniature de la photo sélectionnée
+        _ modification du bouton "valider" lorsque tous les champs
+        du formulaire sont renseigner : bloquer l'envoi sinon et changement
+        de la couleur
+*/
